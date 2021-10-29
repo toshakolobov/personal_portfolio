a7ee4e2ba38ecde417c7ad5f0bf7680ab766a0bd
toshakolobov <toshakolobov@gmail.com>
2021-10-29 17:50:44 +0300

    added page for one blog entry

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Blog
 
 # Create your views here.
@@ -6,3 +6,7 @@ from .models import Blog
 def all_blogs(request):
     entries = Blog.objects.all()
     return render(request, 'blog/all_blogs.html', {'entries': entries})
+
+def detail(request, blog_id):
+    blog = get_object_or_404(Blog, pk=blog_id)
+    return render(request, 'blog/detail.html', {'blog': blog})

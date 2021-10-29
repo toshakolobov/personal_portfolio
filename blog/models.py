8f0c3c4541a3c4043b2eea20e126591690e1325a
toshakolobov <toshakolobov@gmail.com>
2021-10-29 18:15:19 +0300

    added a bit of beauty

--- blog/models.py
+++ blog/models.py
@@ -5,3 +5,6 @@ class Blog(models.Model):
     title = models.CharField(max_length=200)
     date = models.DateField()
     description = models.TextField()
+
+    def __str__(self):
+        return self.title
